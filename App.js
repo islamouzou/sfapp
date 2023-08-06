05a052c625fab7850e5e3461fb6c31b243aa683d
islamouzou <mohammed.islam.zouagui.algerie@viacesi.fr>
2023-08-06 11:02:04 +0100

    Create Salesforce SDK demo app

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { oauth } from "react-native-force";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Button
+        title="Login"
+        onPress={() => {
+          oauth.authenticate();
+        }}
+      ></Button>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ prebuild.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Execute the expo prebuild command
+npx expo prebuild --clean
+
+# Execute the first script
+node ./prebuild-scripts/android-sdk-script.js
+
+# Execute the second script
+node ./prebuild-scripts/build.gradle-script.js
+
+# Execute the third script
+node ./prebuild-scripts/copyscript.js
+
+# Execute the fourth script
+node ./prebuild-scripts/main-activity-script.js
+
+node ./prebuild-scripts/main-application-script.js
+
+node ./prebuild-scripts/settings.gradle-script.js
+
+node ./prebuild-scripts/manifest-script.js
+
+node ./prebuild-scripts/kotlin.js
+
+
+
